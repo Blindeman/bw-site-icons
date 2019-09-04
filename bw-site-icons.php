bb544fd05c9c593560bf1e7fddd894aac3f1cc17
Naomi Blindeman <naomi@blindeman.com>
2019-09-04 12:44:48 -0700

    Update bw-site-icons.php

--- bw-site-icons.php
+++ bw-site-icons.php
@@ -2,6 +2,7 @@
 /**
  * Plugin Name: BW Site Icons
  * Plugin URI: https://blindemanwebsites.com/today-i-learned/
+ * Github plugin URI: https://github.com/Blindeman/bw-site-icons
  * Description: Lightweight and minimal set of icons that one might need on a website
  * Version: 0.0.1
  * Author: Naomi Blindeman
