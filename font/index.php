9d1959f81f1a9c19ef93ec776ccb41b92d619b6a
Naomi Blindeman <naomi@blindeman.com>
2019-09-04 11:16:18 -0700

    Create index.php

---
+++ font/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * To make a prairie it takes a clover and one bee,
+ * One clover, and a bee,
+ * and revery.
+ * The revery alone will do,
+ * If bees are few.
+ * --Emily Dickinson
+ */
