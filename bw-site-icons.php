9422727a7a1c094a6eeab8bd6dd065bd11bec97c
Naomi Blindeman <naomi@blindeman.com>
2019-09-04 11:03:52 -0700

    initial commit

---
+++ bw-site-icons.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Plugin Name: BW Site Icons
+ * Plugin URI: https://blindemanwebsites.com/today-i-learned/
+ * Description: Lightweight and minimal set of icons that one might need on a website
+ * Version: 0.0.1
+ * Author: Naomi Blindeman
+ * Author URI: https://blindemanwebsites.com/
+ * License: GNU General Public License
+ * License URI: https://www.gnu.org/licenses/gpl.html
+ */
+
+defined( 'ABSPATH' ) or die;
+
+//add a stylesheet with the icon font I like
+class a_few_site_icons {
+    /**
+     * The whole reason this class is here is so I can play with them, feels a bit like overkill
+     */
+    function __construct(){
+        add_action( 'wp_enqueue_scripts', array( $this, 'add_icon_styles') );
+    }
+
+    /**
+     * Register & enqueue styles
+     */
+    public function add_icon_styles(){
+        wp_register_style( 'bw-site-icons', plugins_url('bw-site-icons/css/entypo.css'), '', '', 'screen' );
+        wp_enqueue_style( 'bw-site-icons' );
+    }
+}
+
+//instantiate -- call the class to invoke the function inside it
+new a_few_site_icons();
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * "Ik heb zo veel schoonheden in de wereld ontdekt.
+ * Ik dacht dat er minder waren."
+ * --Hans Lodeizen
+ */
